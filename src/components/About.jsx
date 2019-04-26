d8ea741986cf6e1244b990f720a55c870ddee2ea
Porter Savage <theportersavage@gmail.com>
2019-04-26 13:33:28 -0700

    Finished doing basic layout of each component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,10 +10,24 @@ function About() {
             font-family: sans-serif;
             margin-top: 75px;
           }
+          p {
+            font-family: sans-serif;
+            text-align: center;
+            font-size: 50px;
+            border: 2px solid black;
+            margin-right: 20px;
+            margin-left: 20px;
+            padding-right: 8px;
+            padding-left: 8px;
+            background-color: pink;
+          }
         `}</style>
       </div>
       <div className="center">
-        <h1>About!</h1>
+        <h1>About</h1>
+      </div>
+      <div>
+        <p>This Is forum created by Porter Savage. This forum was created for every user's benifit and entertainment.</p>
       </div>
     </div>
   );
--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -5,11 +5,45 @@ function CreatePost() {
     <div>
       <div>
         <style>{`
-
+          .center {
+            font-family: sans-serif;
+            text-align: center;
+            margin-top: 75px;
+          }
+          input, textarea {
+            margin-top: 10px;
+          }
+          .form {
+            text-align: center;
+            border: 2px solid black;
+            margin-right: 500px;
+            margin-left: 500px;
+            padding-top: 10px;
+            padding-bottom: 10px;
+          }
+          textarea {
+            width: 400px;
+            height: 250px;
+          }
+          button {
+            border: 1px solid black;
+            text-align: center;
+          }
         `}</style>
       </div>
-      <div>
-        <h1>CreatePost!</h1>
+      <div className="center">
+        <h1>Create Post</h1>
+      </div>
+      <div className="form">
+        <label htmlFor="title">Title: </label>
+        <input type="text"/>
+        <br/>
+        <label htmlFor="link">Link: </label>
+        <input type="text"/>
+        <br/>
+        <label htmlFor="body">Body: </label>
+        <textarea name="body"></textarea>
+        <button>Post</button>
       </div>
     </div>
   );
